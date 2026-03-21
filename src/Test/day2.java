64cac6043bdb35d6ebc5898fe1e06e167f95c7d9
Pooja Somwanshi <22pooja498@gmail.com>
2026-03-22 00:10:39 +0530

    first commit

---
+++ src/Test/day2.java
@@ -0,0 +1,16 @@
+package Test;
+
+import org.testng.annotations.Test;
+
+public class day2 {
+
+	@Test
+	public void demo() {
+		System.out.println("hello");
+	}
+	@Test
+	public void demo2() {
+		System.out.println("bye");
+	}
+}
+ 
