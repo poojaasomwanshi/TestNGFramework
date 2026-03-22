b1201f17931931dc319dd5de2924072060de0e2d
Pooja Somwanshi <22pooja498@gmail.com>
2026-03-22 14:42:38 +0530

    Exclude Methods Using Exclude Tag

---
+++ src/Test/day3.java
@@ -0,0 +1,21 @@
+package Test;
+
+import org.testng.annotations.Test;
+
+public class day3 {
+
+	@Test
+	public void WebLoginCarLoan() {
+		System.out.println("WebLoginCarLoan");
+	}
+	@Test
+	public void MobileLoginCarLoan() {
+		System.out.println("MobileLoginCarLoan");
+	}
+	@Test
+	public void APILoginCarLoan() {
+		System.out.println("APIloginCarLoan");
+
+		}
+	}
+ 
